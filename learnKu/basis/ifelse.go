230cde3c9520a9cac5db47d2141c954eb99f8ce5
sucd01 <sucd01@mingyuanyun.com>
2020-09-23 10:40:07 +0800

    if语句块测试

--- learnKu/basis/ifelse.go
+++ learnKu/basis/ifelse.go
@@ -1,5 +1,6 @@
 // if-else 结构
 package main
+
 import "fmt"
 
 func main() {
@@ -14,9 +15,31 @@ func main() {
 		fmt.Printf("first is 5 or greater\n")
 	}
 	// if condition的用法
-	if cond =5; cond > 10 {
+	if cond = 5; cond > 10 {
 		fmt.Printf("cond is greater than 10\n")
 	} else {
 		fmt.Printf("cond is not greater than 10\n")
 	}
+
+	supplierLevel, platinumFlag, regionalDiamondLevel, diamondFlag := 1, 1, 1, 0
+	getSupplierLevel(supplierLevel, platinumFlag, regionalDiamondLevel, diamondFlag)
+}
+
+//
+// 获取供应商等级
+//
+func getSupplierLevel(supplierLevel, platinumFlag, regionalDiamondLevel, diamondFlag int) {
+	var memberLevel int32 = 1
+	if supplierLevel == 1 {
+		if diamondFlag == 1 {
+			memberLevel = 5
+		} else if regionalDiamondLevel == 1 {
+			memberLevel = 4
+		} else if platinumFlag == 1 {
+			memberLevel = 3
+		} else {
+			memberLevel = 2
+		}
+	}
+	fmt.Printf("memberLevel is %d\n", memberLevel)
 }
