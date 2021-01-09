5c8922b0aa2226b23699b970356515c0504a4650
sucd01 <sucd01@mingyuanyun.com>
2021-01-09 09:24:42 +0800

    utf8包获取字符串字符个数

--- learnKu/basis/helloworld.go
+++ learnKu/basis/helloworld.go
@@ -1,8 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("hello world")
 	fmt.Println("Go Go Go !!!")
+	str := "hello, 你好"
+	fmt.Println(utf8.RuneCountInString(str))
 }
